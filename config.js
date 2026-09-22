6701e7bc34cd1c4c15f27f60c7d940905b44708d
karenlilianabaron-del <karenlilianabaron@gmail.com>
2026-09-22 09:29:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CE_CONFIG = {
-  url: "https://eogxbwdyxpixkfhmlvem.supabase.co/rest/v1/",
+  url: "https://eogxbwdyxpixkfhmlvem.supabase.co",
   anonKey: "sb_publishable_4yYM7jpzwAkE0n7a4ymOoA_CP7N7RQf"
 };
